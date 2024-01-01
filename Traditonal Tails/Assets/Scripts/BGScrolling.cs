a4558e7a81ce9ec1fd0308c3703e109308448c23
Louisann-Borg <borglouisann0023@gmail.com>
2024-01-01 18:59:16 +0100

    Added Infinite Background Loop
    - Added Background and looping code needs some tweaks but works as intended

---
+++ Traditonal Tails/Assets/Scripts/BGScrolling.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScrolling : MonoBehaviour
+{
+    public float scrollSpeed = 2.0f;
+    public float tileSizeY = 10.0f;
+
+    private Vector2 startPosition;
+
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSizeY
+        transform.position = startPosition + Vector2.up * newPosition;
+    }
+}
